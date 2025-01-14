b3522966a019b6b8e841ad4ca338f5980d82eeca
Iceeyess <ice_eyes@mail.ru>
2025-01-14 11:47:14 +0300

    Commit # 2

--- notifications/models.py
+++ notifications/models.py
@@ -6,9 +6,11 @@ NULLABLE = dict(null=True, blank=True)
 
 # Create your models here.
 class Notification(models.Model):
+    """Класс-модель"""
     message = models.CharField(max_length=1024)
     recipient = models.CharField(max_length=150)
     delay = models.IntegerField()
+    next_send = models.DateTimeField()
     type = models.CharField(max_length=8)
     creation_datetime = models.DateTimeField(auto_now_add=True)
     update_datetime = models.DateTimeField(auto_now=True)
--- notifications/views.py
+++ notifications/views.py
@@ -9,25 +9,29 @@ from notifications.serializers import NotificationSerializer
 # Create your views here.
 class NotificationAPI(views.APIView):
     def post(self, request, *args, **kwargs):
+        """Решил пойти к родительскому классу и верстать в нем, поскольку он без примесей.
+        Данный метод проверяет, является ли поле recipient списком или строкой или иным типом данных.
+        В зависимости от факта, возвращает список из созданных объектов, один созданный объект(случай с одним адресом)
+        или ошибку в валидации данных"""
         data = self.request.data
         recipient = data.get('recipient')
         if isinstance(recipient, list):
-            data_response = list()
+            data_response = list()  # Список для созданных объектов
             for rec in recipient:
                 serializer = NotificationSerializer(
                     data={'recipient': rec, 'message': data.get('message'), 'delay': data.get('delay')})
                 serializer.is_valid(raise_exception=True)
                 serializer.save()
                 data_response.append(serializer.data)
-            Response(data_response, status=201)
+            return JsonResponse(data=data_response, safe=False)
         elif isinstance(recipient, str):
             serializer = NotificationSerializer(data={'recipient': recipient, 'message': data.get('message'), 'delay': data.get('delay')})
             serializer.is_valid(raise_exception=True)
             serializer.save()
-
+            return Response(serializer.data, status=201)
         else:
             return Response({'recipient': ['Получатель должен быть валидным адресом электронной почты или номером Телеграм состоящий только из цифр.']}, status=400)
-        Response(serializer.data, status=201)
+
 
 
 
